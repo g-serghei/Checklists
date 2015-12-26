dccbb6ace1006788d9bc13d3e18c4e69c8304745
Serghei Grigoruta <phoenix92@inbox.ru>
2015-12-26 18:28:05 +0200

    Initial Commit

---
+++ Checklist/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by M.I. Hollemans on 27/07/15.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
